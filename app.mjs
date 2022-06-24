6429cff2c89f4f5faae4b77209d888c4ebdee72d
admin-simandhar <webadmin@simandhareducation.com>
2022-06-24 13:10:21 +0530

    first commit

--- app.mjs
+++ app.mjs
@@ -1,9 +1,10 @@
 import express from "express"
 import { generateUploadURL } from "./s3.mjs"
-
+const cors = require("cors");
 
 const app = express()
-
+app.use(express.json());
+app.use(cors());
 
 
 app.post('/s3Url', async (req, res) => {
