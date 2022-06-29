2b909883f9e0226c0cb20261a2b8141a8af88fcd
admin-simandhar <webadmin@simandhareducation.com>
2022-06-29 18:10:48 +0530

    first commit

--- awsS3Pdf.js
+++ awsS3Pdf.js
@@ -38,6 +38,6 @@ app.post("/upload", (req, res) => {
   run();
 });
 
-app.listen(3000, () => console.log("Server Started..."));
+app.listen(process.env.PORT || 3000, () => console.log("Server Started..."));
 
 
