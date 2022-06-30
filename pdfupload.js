2573fda6da045d05b74d42a38b840683eb888af4
admin-simandhar <webadmin@simandhareducation.com>
2022-06-30 18:02:03 +0530

    first commit

--- pdfupload.js
+++ pdfupload.js
@@ -1,14 +1,14 @@
-const express=require("express")
-const AWS = require('aws-sdk');
-const fs = require('fs');
+const express = require("express");
+const AWS = require("aws-sdk");
+const fs = require("fs");
 const mysql = require("mysql");
-const cors=require("cors") 
-const fileUpload = require('express-fileupload');
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(fileUpload());
-const Buffer=require("buffer");
+const Buffer = require("buffer");
 const { file } = require("pdfkit");
 // const pool=createPool({
 //     host: "127.0.0.1",
@@ -29,22 +29,22 @@ const { file } = require("pdfkit");
 //password: "Jw224j$z",
 // })
 const db = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "test",
-    password: "",
-    database: "simandhar_internal_db",
-  });
-  
-  db.connect(function (err) {
-    if (err) {
-      console.log("Error connecting to Db" + err.stack);
-      return;
-    }
-    console.log("Connection established");
-  }); 
+  host: "localhost",
+  port: 3306,
+  user: "test",
+  password: "",
+  database: "simandhar_internal_db",
+});
 
-app.listen(process.env.PORT || 5000)
+db.connect(function (err) {
+  if (err) {
+    console.log("Error connecting to Db" + err.stack);
+    return;
+  }
+  console.log("Connection established");
+});
+
+app.listen(process.env.PORT || 5000);
 
 // const AWSCredentials = {
 //     accessKey: 'AKIA36ASJN7RA3D6VGJ4',
@@ -57,88 +57,89 @@ app.listen(process.env.PORT || 5000)
 //     secretAccessKey: AWSCredentials.secret
 // });
 
-
-
-
-
-
-
-
 app.post("/query/", async (request, response) => {
-    const { fullname, emailaddress, phonenumber, course, media, textvalue } =
-      request.body; 
+  const { fullname, emailaddress, phonenumber, course, media, textvalue } =
+    request.body;
 
+  AWS.config.update({
+    accessKeyId: "AKIA36ASJN7RA3D6VGJ4", // Access key ID
+    secretAccesskey: "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", // Secret access key
+    region: "ap-south-1", //Region
+  });
 
-      AWS.config.update({
-        accessKeyId: "AKIA36ASJN7RA3D6VGJ4", // Access key ID
-        secretAccesskey: "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", // Secret access key
-        region: "ap-south-1" //Region
-    })
-    
+  let params = {
+    Bucket: "simandhar-edu-assets/Evaluations",
+    ACL: "public-read",
+    Body: media,
+    Key: file.name,
+  }; 
+ 
 
-    let params = {
-        Bucket: "simandhar-edu-assets/Evaluations",
-        Key: "file.name",
-        Body: "file",
-    };
+    const S3_BUCKET ='simandhar-edu-assets/Evaluations';
+  const REGION ='ap-south-1';
+  const myBucket = new AWS.S3({
+   params: { Bucket: S3_BUCKET},
+ region: REGION,
+ }) 
 
-    try {
-        let uploadPromise = await new AWS.S3().putObject(params).promise();
-        console.log("Successfully uploaded data to bucket");
-    } catch (e) {
-        console.log("Error uploading data: ", e);
-    }
-    
-    uploadPromise()
-    // const s3 = new AWS.S3();
-    
-    // Binary data base64
-    // const fileContent  = Buffer.from(request.files.media, 'binary');
-    
-    // Setting up S3 upload parameters
-    // const params = {
-    //     Bucket: 'simandhar-edu-assets/Evaluations',
-    //     Key: fileContent.media, // File name you want to save as in S3
-    //     Body: fileContent 
-    // }; 
-    // s3.upload(params, function(err, media) {
-    //     if (err) {
-    //         throw err;
-    //     }
-    //     res.send({
-    //         "response_code": 200,
-    //         "response_message": "Success",
-    //         "response_data": media
-    //     });
-    // });
-    
+   myBucket.putObject(params)
+    //   .on('httpUploadProgress', (evt) => {
+    //       setProgress(Math.round((evt.loaded / evt.total) * 100))
+          
+    //   })
+      .send((err) => {
+          if (err) console.log(err)
+      })
 
-    //   const uploadToS3 = (media) => {
-    //     // Read content from the file
-    //     const fileContent = fs.readFileSync(media);
-    
-    //     // Setting up S3 upload parameters
-    //     const params = {
-    //         Bucket: AWSCredentials.bucketName,
-    //         Key: media,
-    //         Body: fileContent
-    //     };
-    
-    //     // Uploading files to the bucket
-    //     s3.upload(params, function(err, data) {
-    //         if (err) {
-    //             throw err;
-    //         }
-    //         console.log(`File uploaded successfully. ${data.Location}`);
-    //     });
-    // };
-    
-    // uploadToS3(`${media}`); 
+     
 
 
 
+  // const s3 = new AWS.S3();
 
-    const addUserQuery = `
+  // Binary data base64
+  // const fileContent  = Buffer.from(request.files.media, 'binary');
+
+  // Setting up S3 upload parameters
+  // const params = {
+  //     Bucket: 'simandhar-edu-assets/Evaluations',
+  //     Key: fileContent.media, // File name you want to save as in S3
+  //     Body: fileContent
+  // };
+  // s3.upload(params, function(err, media) {
+  //     if (err) {
+  //         throw err;
+  //     }
+  //     res.send({
+  //         "response_code": 200,
+  //         "response_message": "Success",
+  //         "response_data": media
+  //     });
+  // });
+
+  //   const uploadToS3 = (media) => {
+  //     // Read content from the file
+  //     const fileContent = fs.readFileSync(media);
+
+  //     // Setting up S3 upload parameters
+  //     const params = {
+  //         Bucket: AWSCredentials.bucketName,
+  //         Key: media,
+  //         Body: fileContent
+  //     };
+
+  //     // Uploading files to the bucket
+  //     s3.upload(params, function(err, data) {
+  //         if (err) {
+  //             throw err;
+  //         }
+  //         console.log(`File uploaded successfully. ${data.Location}`);
+  //     });
+  // };
+
+  // uploadToS3(`${media}`);
+
+  const addUserQuery = `
         INSERT INTO query_page( fullname,emailaddress,phonenumber,course,image,textvalue)
         VALUES(
             '${fullname},
@@ -149,7 +150,7 @@ app.post("/query/", async (request, response) => {
             '${textvalue}'
         );
       `;
-    const dbResponse = await db.run(addUserQuery);
-    response.send("New Query Created");
-    response.status(200);
-  });
+  const dbResponse = await db.run(addUserQuery);
+  response.send("New Query Created");
+  response.status(200);
+});
